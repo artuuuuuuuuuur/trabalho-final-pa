48d63d6733b42ef15130c3e902924ca98e1d9fdb
Artur de Maria <arturdemaria7@gmail.com>
2025-07-06 16:54:41 -0300

    criando medicos

--- main.c
+++ main.c
@@ -3,18 +3,80 @@
 #include <stdbool.h>
 #include <windows.h>
 
+typedef struct medico
+{
+    int id;
+    char nome[100];
+    int crm;
+    bool plantao;
+} medico;
+
+void gestaomed(medico medicos[])
+{
+    int num;
+    printf("Você escolheu gestão de médicos! Escolha o que deseja fazer\n\n 1 -- CADASTRAR MÉDICO\n 2 -- CONSULTAR ID\n 3 -- ATUALIZAR MÉDICO\n 4 -- EXCLUIR MÉDICO");
+    scanf("%d", &num);
+    switch (num)
+    {
+    case 1:
+        printf("----- Cadastro de Médicos -------");
+        for (int i = 0; i < 2; i++)
+        {
+            medicos[i].id = i;
+            bool isPlantao = false;
+            char plantao[1];
+            printf("---- Médico %d -----\n", i + 1);
+
+            printf("NOME: ");
+            getchar();
+            fgets(medicos[i].nome, 100, stdin);
+            medicos[i].nome[strcspn(medicos[i].nome, "\n")] = 0;
+
+            printf("CRM: ");
+            scanf("%d", &medicos[i].crm);
+
+            while (!isPlantao)
+            {
+                printf("\nEstá de Plantão? (S / N): ");
+                scanf(" %1s", plantao);
+                if (plantao[0] == 'S' || plantao[0] == 's' || plantao[0] == 'N' || plantao[0] == 'n')
+                {
+                    if (plantao[0] == 'S' || plantao[0] == 's')
+                    {
+                        medicos[i].plantao = true;
+                        isPlantao = true;
+                    }
+                    else if (plantao[0] == 'N' || plantao[0] == 'n')
+                    {
+                        medicos[i].plantao = false;
+                        isPlantao = true;
+                    }
+                }
+                else
+                {
+                    printf("Opção inválida. Digite S (Sim) ou N (Não)");
+                }
+            }
+        }
+    default:
+        break;
+    }
+}
+
 int main() {
+
+    medico medicos[2];
+    
     int num;
     printf("Olá, escolha uma das opções ou feche o programa\n\n 1 -- GESTÃO DE PACIENTES\n 2 -- GESTÃO DE MÉDICOS\n 3 -- FILA DE ATENDIMENTO\n 4 -- SAIR DO SISTEMA\n");
     scanf("%d",&num);
-    while(true){
         switch (num) {
         case 1:
             printf("Gestão de pacientes.\n");
             break;
         case 2:
-            printf("Gestão de médicos.\n");
-            break;
+          gestaomed(medicos);
+          break;
         case 3:
             printf("Fila de Atendimento.\n");
             break;
@@ -26,6 +88,5 @@ int main() {
             printf("Número inválido!");
             break;
         }
-    }
     return 0;
 }
