e89378b40511d51623726bdb327e21051c593603
Yan-Freitas <yf590001@gmail.com>
2025-07-08 23:35:41 -0300

    Update fila.c
    comitado

--- fila.c
+++ fila.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+//Struct que eu ainda não usei e tá montada errada
 typedef struct medico
 {
     int id;
@@ -11,11 +12,13 @@ typedef struct medico
     int idmed;
     int estado;
 }MEDICO;
+//Função de contar a quantidade de médicos
 int medcount ()
 {
    char num[500];
    int nume = 0;
    FILE *fptr;
+    //Trocar nome para só "medico" porque isso tá apontando pra um diretório no meu pc
    fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
    while(fgets(num,500,fptr)){
     nume++;
@@ -23,20 +26,25 @@ int medcount ()
    fclose(fptr);
    return nume;
 }
+//Função principal
 void main()
 {
     char str[100];
     MEDICO medico[1000];
     int nummed = medcount();
     FILE *fptr;
+     //Trocar nome para só "medico" porque isso tá apontando pra um diretório no meu pc
     fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
+    //Função muito ineficiente para postar os médicos por ordem de estado mas foi a única coisa que consegui pensar
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
         if(str[strle]=='3'){
-            printf("%s",str);
+            printf("INTERNACAO IMEDIATA! --> %s",str);
         }
     }
+    fclose(fptr);
+    fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
@@ -44,6 +52,8 @@ void main()
             printf("%s",str);
         }
     }
+    fclose(fptr);
+    fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
