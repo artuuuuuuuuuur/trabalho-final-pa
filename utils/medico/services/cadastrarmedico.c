677dd49f8a6a000b421be4b5608b9e41dbcd091e
LabComp <arturdemaria7@gmail.com>
2025-07-10 10:25:07 -0300

    medico created

--- utils/medico/services/cadastrarmedico.c
+++ utils/medico/services/cadastrarmedico.c
@@ -13,48 +13,76 @@ typedef struct medico
   bool plantao;
 } medico;
 
+void cadastrarMedico(int currentId, FILE *medicos)
+{
+  printf("----- Cadastro de Médicos -------");
+  bool cadastroBool = true;
+  int i = 0;
+  while (cadastroBool)
+  {
+    i++;
+    medico medico;
+    medico.id = currentId + i;
+    bool isPlantao = false;
+    char plantao[1];
 
-void cadastrarMedico(int currentId, FILE *medicos) {
-    printf("----- Cadastro de Médicos -------");
-    for (int i = 1; i < 4; i++)
+    printf("NOME: ");
+    getchar();
+    fgets(medico.nome, 100, stdin);
+    medico.nome[strcspn(medico.nome, "\n")] = 0;
+
+    printf("CRM: ");
+    scanf("%d", &medico.crm);
+
+    while (!isPlantao)
     {
-      medico medico;
-      medico.id = currentId + i;
-      bool isPlantao = false;
-      char plantao[1];
-      printf("---- Médico %d -----\n", i);
-
-      printf("NOME: ");
-      getchar();
-      fgets(medico.nome, 100, stdin);
-      medico.nome[strcspn(medico.nome, "\n")] = 0;
-
-      printf("CRM: ");
-      scanf("%d", &medico.crm);
-
-      while (!isPlantao)
+      printf("\nEstá de Plantão? (S / N): ");
+      scanf(" %1s", plantao);
+      if (plantao[0] == 'S' || plantao[0] == 's' || plantao[0] == 'N' || plantao[0] == 'n')
       {
-        printf("\nEstá de Plantão? (S / N): ");
-        scanf(" %1s", plantao);
-        if (plantao[0] == 'S' || plantao[0] == 's' || plantao[0] == 'N' || plantao[0] == 'n')
+        if (plantao[0] == 'S' || plantao[0] == 's')
         {
-          if (plantao[0] == 'S' || plantao[0] == 's')
-          {
-            medico.plantao = true;
-            isPlantao = true;
-          }
-          else if (plantao[0] == 'N' || plantao[0] == 'n')
-          {
-            medico.plantao = false;
-            isPlantao = true;
-          }
+          medico.plantao = true;
+          isPlantao = true;
         }
-        else
+        else if (plantao[0] == 'N' || plantao[0] == 'n')
         {
-          printf("Opção inválida. Digite S (Sim) ou N (Não)");
+          medico.plantao = false;
+          isPlantao = true;
         }
       }
-
-      fprintf(medicos, "\n%d, %s, %d, %s", medico.id, medico.nome, medico.crm,  (medico.plantao == 1 ? "true" : "false"));
+      else
+      {
+        printf("Opção inválida. Digite S (Sim) ou N (Não)");
+      }
     }
+
+    char cadastro[1];
+    bool response = false;
+    while (!response)
+    {
+      getchar();
+      printf("\nDeseja cadastrar outro médico? (S / N): ");
+      scanf("%c", cadastro);
+      if (cadastro[0] == 'S' || cadastro[0] == 's' || cadastro[0] == 'N' || cadastro[0] == 'n')
+      {
+        if (cadastro[0] == 'S' || cadastro[0] == 's')
+        {
+          cadastroBool = true;
+          response = true;
+        }
+        else if (cadastro[0] == 'N' || cadastro[0] == 'n')
+        {
+          cadastroBool = false;
+          response = true;
+        }
+      }
+      else
+      {
+        printf("Opção inválida. Digite S (Sim) ou N (Não)");
+      }
+    }
+
+    fprintf(medicos, "\n%d, %s, %d, %s", medico.id, medico.nome, medico.crm, (medico.plantao == 1 ? "true" : "false"));
+  }
 }
--- utils/medico/services/cadastrarmedico.h
+++ utils/medico/services/cadastrarmedico.h
@@ -1,6 +1,5 @@
 #ifndef CADASTRARMEDICO_H
 #define CADASTRARMEDICO_H
-#include "cadastrarMedico.h"
 
 void cadastrarMedico(int currentId, FILE *medicos);
 
