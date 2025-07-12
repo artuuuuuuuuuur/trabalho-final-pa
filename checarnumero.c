18f350e50a664d7cd5dc51ab8359dc3ff6f56b08
Yan-Freitas <yf590001@gmail.com>
2025-07-12 20:51:07 -0300

    Update checarnumero.c

--- checarnumero.c
+++ checarnumero.c
@@ -4,40 +4,40 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+//Função que checa o número do médico
 int checknum(char med[]){
     int buf = 0;
     int num = 0;
     char strnum[50];
     int numret;
+    //A mágica é feita aqui
     for(int i = 0;i<500;i++)
     {
         buf=i;
+        //Função isdigit() checa se é inteiro;
         if(isdigit(med[i])){
+            //Vai checar se é inteiro até não ser;
             while(isdigit(med[i])){
+                //Vai aplicar todos os inteiros á um array de char;
                 strnum[num]=med[buf];
                 num++;
                 buf++;
             }
+            //A função atoi vai converter a string em um inteiro, só que ela só funciona em Windows então precisa adicionar o código dela na função em OS diferente.
             numret=atoi(strnum);
+            //Retorna a string convertida em inteiro
             return(numret);
         };
     }
 }
 void main(){
     FILE *fptr;
-    FILE *fbuffer;
-    int num;
-    fbuffer = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
+    //Abertura do arquivo,trocar apenas para "medico"
     fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico","r");
     char nome[500];
-    char buffer[500];
-    char medcu[500]="MEDICO10: ID:1009,NOME:Jose,IDADE:76,IDMED:1009,ESTADO:1";
-    fgets(nome,500,fbuffer);
-    fgets(buffer,500,fptr);
-    fclose(fbuffer);
+    fgets(nome,500,fptr);
+    //Só pode aplicar a função após o fgets ser feito,pois ela é dependente de uma string
+    int num=checknum(medcu);
     fclose(fptr);  
-    num=checknum(medcu);
-    printf("%d\n",num);
-    printf("%s",buffer);
     printf("%s",nome);
 }
