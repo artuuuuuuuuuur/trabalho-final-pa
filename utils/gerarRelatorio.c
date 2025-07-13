0c0357419e1e2f4de7482c8d922ebb3faf7778d2
Artur de Maria <arturdemaria7@gmail.com>
2025-07-13 18:52:06 -0300

    feature/relatorio-final: criando arquivos separados por dia

--- utils/gerarRelatorio.c
+++ utils/gerarRelatorio.c
@@ -1,17 +1,44 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/stat.h>
 
 void gerarRelatorio()
 {
   FILE *relatorio;
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  
+  char year[5];
+  char month[3];
+  char day[3];
+  
+  sprintf(year, "%d", (t->tm_year + 1900));
+  sprintf(month, "%d", (t->tm_mon + 1));
+  sprintf(day, "%d", t->tm_mday);
 
-  relatorio = fopen("relatorio.txt", "w");
+  char filename[30] = "relatorio_";
+
+  strcat(filename, day);
+  strcat(filename, "-");
+  strcat(filename, month);
+  strcat(filename, "-");
+  strcat(filename, year);
+
+  
+  strcat(filename, ".txt");
+
+  relatorio = fopen(filename, "w");
   if (relatorio == NULL)
   {
-    printf("NULOOO");
+    printf("Não foi possível executar a tarefa.");
   }
-
-  fputs("Hello World", relatorio);
+  
+  fputs("Relatório Geral do Sistema XXXX\n", relatorio);
+  fputs("- Total de Pacientes: \n", relatorio);
+  fputs("- Total de Pacientes: ", relatorio);
+  
   fclose(relatorio);
+  printf("Relatório '%s' criado.", filename);
 }
