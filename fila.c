f529b6eeb51cda0481d67caf8b9dab0f9fbbe9c8
Yan-Freitas <yf590001@gmail.com>
2025-07-19 21:33:20 -0300

    Update fila.c
    agora cria arquivo de texto organizado

--- fila.c
+++ fila.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+//Checar quantidade de medicos
 int medcount ()
 {
    char num[500];
@@ -20,31 +21,42 @@ void main()
     char str[100];
     int nummed = medcount();
     FILE *fptr;
+    FILE *forg;
     fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\paciente","r");
+    //Forg vai ser usado pra criar um novo arquivo de texto com os pacientes organizados em ordem de estado
+    forg = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\pacienteorganizado","w");
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
         if(str[strle]=='3'){
             printf("\033[31mINTERNACAO IMEDIATA! -->\033[0m %s",str);
+            fprintf(forg,"INTERNAÇÃO IMEDIATA! %s",str);
         }
     }
+    fclose(forg);
     fclose(fptr);
+    forg = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\pacienteorganizado","a");
     fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\paciente","r");
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
         if(str[strle]=='2'){
             printf("%s",str);
+            fprintf(forg,"%s",str);
         }
     }
+    fclose(forg);
     fclose(fptr);
+    forg = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\pacienteorganizado","a");
     fptr = fopen("C:\\Users\\Public\\Documents\\Trabalho\\Pacientes\\paciente","r");
     for(int i=0;i<nummed;i++){
         fgets(str,100,fptr);
         int strle = strlen(str)-2;
         if(str[strle]=='1'){
             printf("%s",str);
+            fprintf(forg,"%s",str);
         }
     }
+    fclose(forg);
     fclose(fptr);
 }
