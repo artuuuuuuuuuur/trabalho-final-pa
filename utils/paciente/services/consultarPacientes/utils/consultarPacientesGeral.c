de3fd1c13c778636956681a43745169f23a25b71
Artur de Maria <arturdemaria7@gmail.com>
2025-07-20 20:57:35 -0300

    feature/consultar-pacientes: consulta geral da lista de pacientes

--- utils/paciente/services/consultarPacientes/utils/consultarPacientesGeral.c
+++ utils/paciente/services/consultarPacientes/utils/consultarPacientesGeral.c
@@ -13,17 +13,21 @@ void consultarPacientesGeral(FILE *pacientes) {
   printf("-------------------------- Lista Geral de Pacientes --------------------------\n\n");
   // Lê linha por linha
   while (fgets(linha, sizeof(linha), pacientes) != NULL) {
-    if (strcmp(linha, "id,nome,crm,plantao\n") != 0) {
+    if (strcmp(linha, "id,nome,cpf,idade,idmed,estado\n") != 0) {
         char id[100];
         char nome[100];
-        char crm[100];
-        char plantao[100];
+        char cpf[100];
+        char idade[100];
+        char idmed[100];
+        char estado[100];
         strcpy(id,strtok(linha, ","));
         strcpy(nome, strtok(NULL, ","));
-        strcpy(crm, strtok(NULL, ","));
-        strcpy(plantao, strtok(NULL, ","));
+        strcpy(cpf, strtok(NULL, ","));
+        strcpy(idade, strtok(NULL, ","));
+        strcpy(idmed, strtok(NULL, ","));
+        strcpy(estado, strtok(NULL, ","));
 
-        printf("[%s]\n  Nome: %s\n  CRM: %s\n  Plantão: %s\n", id, nome, crm, plantao);
+        printf("[%s]\n  Nome: %s\n  CPF: %s\n  IDADE: %s\n  ID DO MÉDICO RESPONSÁVEL: %s\n  ESTADO: %s\n", id, nome, cpf, idade, idmed, estado);
         printf("----------------------------------------------------------------------\n");
     }
   }
