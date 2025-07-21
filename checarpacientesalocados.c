2d118e611a6b4074688eecff434d60e2b0fdbea3
Yan-Freitas <yf590001@gmail.com>
2025-07-21 16:59:07 -0300

    Update checarpacientesalocados.c

--- checarpacientesalocados.c
+++ checarpacientesalocados.c
@@ -5,15 +5,16 @@
 #include <ctype.h>
 int checkidmed(char med[]){
     int buffer = 0;
-    int buffer2;
-    int buffer3;
+    int buffer2 = 0;
+    int buffer3 = 0;
+    int buffer4;
     int num = 0;
     char strnum[50];
     int numret;
     //Primeiro loop pra passar pela checagem do numero do medico
     for(int i = 0;i<500;i++)
     {
-        buffer=i;
+        buffer++;
         if(isdigit(med[i])){
             while(isdigit(med[buffer])){
                 buffer++;
@@ -21,32 +22,46 @@ int checkidmed(char med[]){
             i=500;
         }
     }
-    buffer++;
+    buffer+=5;
+    buffer2=buffer;
     //Segundo loop pra checar o id
     for(int i = buffer;i<500;i++)
     {
-        buffer2=i;
         if(isdigit(med[i])){
             while(isdigit(med[buffer2])){
                 buffer2++;
             }
         }
+        buffer2++;
         i=500;
     }
     buffer2++;
+    buffer3=buffer2;
     //Terceiro loop pra checar o id do médico
     for(int i = buffer2;i<500;i++)
     {
-        buffer3=i;
         if(isdigit(med[i])){
             while(isdigit(med[buffer3])){
-                strnum[num]=med[buffer3];
-                num++;
                 buffer3++;
+            }
+            break;
+        }
+        buffer3++;
+    }
+    buffer3++;
+    buffer4=buffer3;
+    for(int i = buffer3;i<500;i++)
+    {
+        if(isdigit(med[i])){
+            while(isdigit(med[buffer4])){
+                strnum[num]=med[buffer4];
+                num++;
+                buffer4++;
             }
             numret=atoi(strnum);
             return numret;
         }
+        buffer4++;
     }
 }
 void main()
@@ -64,11 +79,7 @@ void main()
     printf("Esse medico esta alocado aos pacientes:\n");
     while(fgets(str,100,fptr)){
         if(idmed==checkidmed(str)){
-            while(idmed==checkidmed(str)){
-                printf("%s",str);
-                fgets(str,100,fptr);
-            }
-            break;
+            printf("%s",str);
         }
     }
     fclose(fptr);
