b2893c0dad419f8f2d201c249bea37bf6f04a422
Yan-Freitas <yf590001@gmail.com>
2025-07-21 17:01:19 -0300

    Update checaridmed.c

--- checaridmed.c
+++ checaridmed.c
@@ -2,51 +2,66 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-//Função de checar ID
-int checkid(char pac[]){
+int checkid(char med[]){
     int buffer = 0;
-    int buffer2;
-    int buffer3;
+    int buffer2 = 0;
+    int buffer3 = 0;
+    int buffer4;
     int num = 0;
     char strnum[50];
     int numret;
     //Primeiro loop pra passar pela checagem do numero do medico
     for(int i = 0;i<500;i++)
     {
-        buffer=i;
-        if(isdigit(pac[i])){
-            while(isdigit(pac[buffer])){
+        buffer++;
+        if(isdigit(med[i])){
+            while(isdigit(med[buffer])){
                 buffer++;
             }
             i=500;
         }
     }
-    buffer++;
+    buffer+=5;
+    buffer2=buffer;
     //Segundo loop pra checar o id
     for(int i = buffer;i<500;i++)
     {
-        buffer2=i;
-        if(isdigit(pac[i])){
-            while(isdigit(pac[buffer2])){
+        if(isdigit(med[i])){
+            while(isdigit(med[buffer2])){
                 buffer2++;
             }
         }
+        buffer2++;
         i=500;
     }
     buffer2++;
-    //Terceiro loop pra checar o id do médico
+    buffer3=buffer2;
+    //Terceiro loop pra checar a idade do paciente
     for(int i = buffer2;i<500;i++)
     {
-        buffer3=i;
-        if(isdigit(pac[i])){
-            while(isdigit(pac[buffer3])){
-                strnum[num]=pac[buffer3];
-                num++;
+        if(isdigit(med[i])){
+            while(isdigit(med[buffer3])){
                 buffer3++;
+            }
+            break;
+        }
+        buffer3++;
+    }
+    buffer3++;
+    buffer4=buffer3;
+    //O QUARTO LOOP PEGA O IDMED DO PACIENTE
+    for(int i = buffer3;i<500;i++)
+    {
+        if(isdigit(med[i])){
+            while(isdigit(med[buffer4])){
+                strnum[num]=med[buffer4];
+                num++;
+                buffer4++;
             }
             numret=atoi(strnum);
             return numret;
         }
+        buffer4++;
     }
 }
 void main(){
@@ -56,7 +71,7 @@ void main(){
     char nome[500];
     while(fgets(nome,500,fptr)){
         num=checkid(nome);
-        printf("%d\n",num);
+    printf("%d\n",num);
     }
     fclose(fptr);  
 }
